babc03e63bd533bf3969f347a490286cbb2d39ef
Michael F. Herbst <info@michael-herbst.com>
2016-06-01 12:39:18 +0200

    Typo and formatting corrections by Adam

--- README.pdf.sh
+++ README.pdf.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 BASE=$(basename "$0" ".pdf.sh")
 MD="$BASE.md"
-pandoc --smart --number-sections --standalone --toc -V "linkcolor:red" -M geometry="scale=.8" -o "$BASE.pdf" "$BASE.md"
+pandoc --smart --number-sections --standalone --toc -V "linkcolor:red" \
+	-M geometry="scale=.8" -o "$BASE.pdf" "$@" "$BASE.md"
